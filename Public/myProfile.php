ae20569ffa0b4676fc7e11c7d92e3b49a4dfa803
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-08-29 18:26:33 +0200

    Update myProfile.php

--- Public/myProfile.php
+++ Public/myProfile.php
@@ -12,34 +12,7 @@
 </head>
 <body>
 
-  <header>
-    <div class="jumbotron">
-        <div class="media">
-            <img id="lib_pic" src="Images/Library_logo.jpg" alt="Library_sign" class="align-self-center mr-3">
-            <div class="align-self-center mr-3">
-              <h1>My Library</h1>
-            </div>
-          </div>
-          <p style="text-align:right;"><a href="logout.php">Logout</a></p>
-    </div>
-    
-    <nav>
-      <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="home.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="myProfile.php">My Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="books.php">Books</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-        </ul>
-    </nav>
-  </header>
+  <?php include('header.php'); ?>
 <?php
 
 include('../Private/configuration.php');
@@ -85,15 +58,6 @@ $user = $result->fetch_array();
 </div>
 
 
-  <footer>
-    <div class="footer">
-      <pre>
-           My Library
-           Adress
-           Phone
-           Email adress
-      </pre>
-    </div>
-  </footer>
+  <?php include('footer.php'); ?>
 </body>
 </html>  
