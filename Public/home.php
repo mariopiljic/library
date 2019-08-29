2acf2db67fa40f840a9158ca490a6aa6211418c8
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-08-29 18:25:39 +0200

    Update home.php

--- Public/home.php
+++ Public/home.php
@@ -13,34 +13,7 @@
 </head>
 <body>
 
-  <header>
-    <div class="jumbotron">
-        <div class="media">
-            <img id="lib_pic" src="Images/Library_logo.jpg" alt="Library_sign" class="align-self-center mr-3">
-            <div class="align-self-center mr-3">
-              <h1>My Library</h1>
-            </div>
-          </div>
-          <p style="text-align:right;"><a href="logout.php">Logout</a></p>
-    </div>
-    
-    <nav>
-      <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="home.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="myProfile.php">My Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="books.php">Books</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-        </ul>
-    </nav>
-  </header>
+  <?php include('header.php'); ?>
   
   <div class="slider">
     <div><img class="lib_pic" src="Images/Library1.jpg"></div>
@@ -48,16 +21,7 @@
     <div><img class="lib_pic" src="Images/Library3.jpg"></div>
   </div>
 
-  <footer>
-    <div class="footer">
-      <pre>
-           My Library
-           Adress
-           Phone
-           Email adress
-      </pre>
-    </div>
-  </footer>
+  <?php include('footer.php'); ?>
 
   <script>
     $('.slider').bxSlider({
