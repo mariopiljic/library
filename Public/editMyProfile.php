8aab32a8351f139baf75f6057ed391447865b299
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-09-06 15:12:26 +0200

    Update editMyProfile.php

--- Public/editMyProfile.php
+++ Public/editMyProfile.php
@@ -46,11 +46,11 @@
                   </div>";
           } else{
               echo "<div class='form'>
-              <h3>Please use different username.</h3>"
+              <h3>Please use different username.</h3>
               <p>Click here to go back</p>
               <a style='width:80px;' href='myProfile.php' role='button' class='btn btn-primary' name='edit_btn'>Back</a> 
               </div>";
-            }
+          }
         
   } else{
 ?>
