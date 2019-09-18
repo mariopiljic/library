b22cbb7e304aa8a51dc4efd801517944dfab2f00
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-09-18 13:28:10 +0200

    Update adminindex.php

--- Private/adminindex.php
+++ Private/adminindex.php
@@ -1,22 +1,36 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script  src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+  <link href="../Public/library.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <link href="../Public/library.css" rel="stylesheet">
 </head>
 <body>
-    <div>
-    <p>Welcome <?php echo htmlspecialchars($_SESSION['username']); ?>!</p>
-    <p>You are logged in as admin.</p>
-    <p>This is My Library</p>
-    <p><a href="adminhome.php">Home</a></p>
-    <a href="../Public/logout.php">Logout</a>
-    </div>
+
+  <?php include('adminheader.php'); ?>
+  
+  <?php 
+       include('configuration.php');    
+       $query = "SELECT * FROM `books`";
+       $result = $db->query($query);
+       $row = mysqli_num_rows($result);
+  ?>
+
+  <div id="minh">
+    <p>Welcome <?php echo  htmlspecialchars($_SESSION['username']); ?>!</p>
+    <p>This is My Library.</p>
+    <p>You are logged as admin.</p>
+    <p>Currently you have <?php echo $row; ?> books.</p>
+  </div>
+  
+  <?php include('adminfooter.php'); ?>
 </body>
 </html>
