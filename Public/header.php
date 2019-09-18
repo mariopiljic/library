e9ebe65edabeac8da69a5f67f14bc92100833a76
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-09-18 13:16:06 +0200

    Update header.php

--- Public/header.php
+++ Public/header.php
@@ -1,18 +1,24 @@
 <header>
-    <div class="jumbotron">
-        <div class="media">
-            <img style="max-width:40%;height:auto;" id="lib_pic" src="Images/Library_logo.jpg" alt="Library_sign" class="align-self-center mr-3">
-            <div class="align-self-center mr-3">
-              <h1 style="font-size:5vw;">My Library</h1>
-            </div>
+    <div id="demo" class="carousel slide" data-ride="carousel">
+      <div  class="carousel-inner" >
+        <div class="carousel-item active" >
+          <img style="width:100%; height:220px;" src="Images/Library1.jpg">
         </div>
-        <p style="text-align:right;"><a href="logout.php" role="button" class="btn btn-danger">Logout</a></p>
+        <div class="carousel-item">
+          <img style="width:100%; height:220px;" src="Images/Library4.jpg">
+        </div>
+        <div class="carousel-item">
+          <img style="width:100%; height:220px;"  src="Images/Library5.jpg" >
+        </div>
+        <div class="centered"><h1 style="font-size:6vw;">My Library</h1></div>
+        <div class="bottom-right"><a href="logout.php" role="button" class="btn btn-danger">Logout</a></div>
+      </div>
     </div>
     
     <nav>
       <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="home.php">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="myProfile.php">My Profile</a>
