a4acaed55ad3bb2407ccfb2e494d09603b70e63f
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-09-19 16:29:25 +0200

    Add files via upload

---
+++ Public/contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script  src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+  <link href="library.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<div style="padding-top:2em;" class="container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11361.
+                177511905573!2d18.644411237707793!3d44.61145579106476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+                1!3m3!1m2!1s0x475eabc0df8034e9%3A0xd705c13311cfc67d!2zQnJlxaFrZQ!5e0!3m2!1sen!2sba!4v1568898142777!5m2!1sen!2sba" 
+                width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="">
+    </iframe>
+</div>
+
+<div style="padding:2em;" class="table-responsive">
+  <h2>Contact</h2>
+    <ul class="list-group">
+        <li class="list-group-item">Adress: Breske 10,75000 Tuzla</li>
+        <li class="list-group-item">Phone: +38735295221</li>
+        <li class="list-group-item">Mobile: +38762511473</li>
+        <li class="list-group-item">Email: mario.piljic89@gmail.com</li>
+        <li class="list-group-item">Facebook: <a href="https://www.facebook.com/mario.piljic">https://www.facebook.com/mario.piljic</a></li>
+    </ul>
+</div>
+
+<?php include('footer.php') ?>
+</body>
+</html>  
