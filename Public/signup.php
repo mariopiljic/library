8af8f9e142612e52e6c930ab5fde331cb5eff543
mariopiljic <52963027+mariopiljic@users.noreply.github.com>
2019-09-20 14:56:42 +0200

    Update signup.php

--- Public/signup.php
+++ Public/signup.php
@@ -27,10 +27,44 @@
     $result = $db->query($query);
     $rows = mysqli_num_rows($result);
     if($rows == 1){
-      echo "<div id='minh'>
-                <h3>Please use different username.</h3>
-                <a data-toggle='tooltip' title='Click here to try again.' style='width:80px;' href='signup.php' role='button' class='btn btn-primary' name='signup_btn'>Signup</a> 
-            </div>";
+                    echo "
+                      <div id='signup' class='container'>
+                        <h2 style='color:red;'>Please use different username!</h2>
+                        <form action='' class='was-validated' method='post'>
+                          <div class='form-group'>
+                            <label for='firstname'>FIrstname:</label>
+                            <input type='text' class='form-control' id='firstname' placeholder='Enter firstname' name='firstname' required>
+                            <div class='valid-feedback'>Valid.</div>
+                            <div class='invalid-feedback'>Please enter firstname.</div>
+                          </div>
+                          <div class='form-group'>
+                            <label for='lastname'>Lastname:</label>
+                            <input type='text' class='form-control' id='lastname' placeholder='Enter lastname' name='lastname' required>
+                            <div class='valid-feedback'>Valid.</div>
+                            <div class='invalid-feedback'>Please enter lastname.</div>
+                          </div>
+                          <div class='form-group'>
+                            <label for='username'>Username:</label>
+                            <input type='text' class='form-control' id='username' placeholder='Enter username' name='username' required>
+                            <div class='valid-feedback'>Valid.</div>
+                            <div class='invalid-feedback'>Please enter username.</div>
+                          </div>
+                          <div class='form-group'>
+                            <label for='email'>Email address:</label>
+                            <input type='email' class='form-control' id='email' placeholder='Enter Email' name='email' required>
+                            <div class='valid-feedback'>Valid.</div>
+                            <div class='invalid-feedback'>Please enter Email.</div>
+                          </div>
+                          <div class='form-group'>
+                            <label for='password'>Password:</label>
+                            <input type='password' class='form-control' id='password' placeholder='Enter password' name='password' required>
+                            <div class='valid-feedback'>Valid.</div>
+                            <div class='invalid-feedback'>Please enter password.</div>
+                          </div>
+                          <button style='width:80px;'type='submit' class='btn btn-primary' name='signup_btn'>Signup</button>
+                          <a style='float:right; width:80px;' href='login.php' role='button' class='btn btn-primary' name='login_btn'>Login</a>
+                        </form>
+                      </div>";
     } else{
 
       
@@ -39,10 +73,28 @@
       $result = $db->query($query);
       
           if($result){
-            echo "<div id='minh'>
-                      <h3>You are registered successfully.</h3>
-                      <a data-toggle='tooltip' title='Click here to login.' style='width:80px;' href='login.php' role='button' class='btn btn-primary' name='login_btn'>Login</a> 
-                  </div>";
+                      echo "
+                        <div id='login' class='container'>
+                          <h2 style='color:red;'>You are now registered.</h2>
+                          <p>Please login.</p>
+                          <form action='login.php' class='was-validated' method='post'>
+                      
+                            <div class='form-group'>
+                              <label for='username'>Username:</label>
+                              <input type='text' class='form-control' id='username' placeholder='Enter username' name='username' required>
+                              <div class='valid-feedback'>Valid.</div>
+                              <div class='invalid-feedback'>Please enter username.</div>
+                            </div>
+                            <div class='form-group'>
+                              <label for='password'>Password:</label>
+                              <input type='password' class='form-control' id='password' placeholder='Enter password' name='password' required>
+                              <div class='valid-feedback'>Valid.</div>
+                              <div class='invalid-feedback'>Please enter password.</div>
+                            </div>
+                            <button style='width:80px;' type='submit' class='btn btn-primary' name='login_btn'>Login</button>
+                            <a style='float:right; width:80px;' href='signup.php' role='button' class='btn btn-primary' name='signup_btn'>Signup</a>
+                          </form>
+                        </div>";
           } 
       }      
   } else{
